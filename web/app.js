cddf8fea4e998f3047bbfa9133b0d5fd0568c9fb
Ahlul <ahlul963@gmail.com>
2024-07-04 03:37:10 +0700

    beta dashboard admin

---
+++ web/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const apiRouter = require("./routes/api");
+const { vault } = require("../bot/utils/vaultUtils");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index", { vault });
+});
+
+app.use("/api", apiRouter);
+
+app.listen(port, () => {
+  console.log(`Web admin dashboard listening at http://localhost:${port}`);
+});
---
+++ web/routes/api.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const { vault, saveVaultData } = require("../../bot/utils/vaultUtils");
+
+router.post("/edit-item", (req, res) => {
+  const { category, item, quantity } = req.body;
+
+  if (!vault.hasOwnProperty(category)) {
+    return res
+      .status(400)
+      .json({ error: `Kategori ${category} tidak ada dalam brankas.` });
+  }
+
+  if (!vault[category].hasOwnProperty(item)) {
+    return res
+      .status(400)
+      .json({ error: `Item ${item} tidak ada dalam kategori ${category}.` });
+  }
+
+  vault[category][item] = parseInt(quantity, 10);
+  saveVaultData();
+
+  return res.json({
+    success: true,
+    message: `Item ${item} di kategori ${category} telah diperbarui dengan jumlah ${quantity}.`,
+  });
+});
+
+module.exports = router;
