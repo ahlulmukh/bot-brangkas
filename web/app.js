c9e697322fb49a2e69c5e82fd4dcc5f3074fd942
Ahlul <ahlul963@gmail.com>
2024-07-04 18:55:13 +0700

    update port

--- web/app.js
+++ web/app.js
@@ -5,7 +5,7 @@ const apiRouter = require("./routes/api");
 const { vault } = require("../bot/utils/vaultUtils");
 
 const app = express();
-const port = 3000;
+const port = 1955;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
