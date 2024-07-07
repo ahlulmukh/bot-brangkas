52559aa96ca192af18d1b0175aa0cd7879da1460
Ahlul <ahlul963@gmail.com>
2024-07-08 01:56:31 +0700

    fix edit

--- web/routes/api.js
+++ web/routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Vault = require("../../models/Vault");
 
 // API Endpoint untuk mengedit data brankas
+// API Endpoint untuk mengedit data brankas
 router.post("/edit-item", async (req, res) => {
   const { category, item, quantity } = req.body;
 
@@ -45,11 +46,31 @@ router.get("/check-updates", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+// Endpoint untuk cek perubahan data
+router.get("/check-updates", async (req, res) => {
+  try {
+    const vault = await Vault.findOne();
+    res.json(vault);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 router.get("/vault-data", async (req, res) => {
   try {
     const vault = await Vault.find();
     res.json(vault);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.get("/table", async (req, res) => {
+  try {
+    const vault = await Vault.findOne();
+    res.render("partials/table", { vault });
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
